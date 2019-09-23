13830f41ecb725ed8713de6e4f599f982e5072f7
Iain Diamond <iain.diamond@gmail.com>
2019-09-23 13:52:26 +0100

    Display time in minutes and seconds

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,13 @@ import React from 'react'
 import styles from './Display.module.css'
 
 const Display = ({ seconds }) => {
-  return <div className={styles.display}>{seconds}</div>
+  const mins = Math.floor(seconds / 60)
+  const secs = String(seconds % 60).padStart(2, '0')
+  return (
+    <div className={styles.display}>
+      {mins}:{secs}
+    </div>
+  )
 }
 
 export default Display

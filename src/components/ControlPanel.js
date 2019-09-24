e2dd891664f51a29f541239ed85a54d16098d555
Iain Diamond <iain.diamond@gmail.com>
2019-09-24 12:55:04 +0100

    Add click handlers to control panel

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react'
 import styles from './ControlPanel.module.css'
 
 class ControlPanel extends Component {
+  state = {
+    running: false,
+  }
+  constructor() {
+    super()
+    this.toggleRunning = this.toggleRunning.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+  toggleRunning() {
+    console.log('toggle running')
+    this.setState({
+      running: !this.state.running,
+    })
+  }
+  reset() {
+    console.log('reset')
+  }
   render() {
+    const { running } = this.state
     return (
       <div className={styles.controlPanel}>
-        <span>
-          <img src="./icons/play-arrow.svg" alt="start timer" />
-        </span>
-        <span>
-          <img src="./icons/pause-button.svg" alt="pause timer" />
-        </span>
-        <span>
+        {!running && (
+          <span onClick={this.toggleRunning}>
+            <img src="./icons/play-arrow.svg" alt="start timer" />
+          </span>
+        )}
+        {running && (
+          <span onClick={this.toggleRunning}>
+            <img src="./icons/pause-button.svg" alt="pause timer" />
+          </span>
+        )}
+        <span onClick={this.reset}>
           <img src="./icons/reset.svg" alt="reset timer" />
         </span>
       </div>

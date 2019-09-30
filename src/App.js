e8f0d76534f7b81332000354af556c42279752f8
Iain Diamond <iain.diamond@gmail.com>
2019-09-30 13:28:46 +0100

    Obscure session controls while running

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Activity from './components/Activity'
 import Clock from './components/Clock'
 import ControlPanel from './components/ControlPanel'
 import Session from './components/Session'
 import './App.css'
 
-function App() {
+function App(props) {
+  const opacity = props.running ? '0.2' : '1'
   return (
     <div className="App">
-      <div id="sessions">
+      <div style={{ opacity }} id="sessions">
         <Session title="session Length" pomodoro />
         <Session title="break Length" />
       </div>
@@ -19,4 +21,8 @@ function App() {
   )
 }
 
-export default App
+const mapStateToProps = state => ({
+  running: state.running,
+})
+
+export default connect(mapStateToProps)(App)
